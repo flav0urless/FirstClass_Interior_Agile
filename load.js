8bc31e20fee589e2114b645115bab5626781cb26
Rxkz <rxkz.xv@icloud.com>
2023-11-03 11:36:59 +1300

    Updated Loader

--- load.js
+++ load.js
@@ -1,49 +1,41 @@
+// Function to hide the preloader 
+function hidePreloader() {
+  const preloader = document.getElementById("preloader");
+  setTimeout(() => {
+    preloader.style.display = "none";
+  }, 2000); //load dell
+}
 
-// Function to update the progress bar
 function updateProgressBar() {
-    const progressBar = document.getElementById("progress-bar");
-    let width = 0;
+  const progressBar = document.getElementById("progress-bar");
+  let width = 0;
 
-    
-    const interval = setInterval(() => {
-      if (width >= 100) {
-        clearInterval(interval); 
-        hideProgressBar();
-      } else {
-        width++;
-        progressBar.style.width = width + "%";
-      }
-    }, 15); 
-  }
+  const interval = setInterval(() => {
+    if (width >= 100) {
+      clearInterval(interval);
+      hidePreloader(); 
+    } else {
+      width++;
+      progressBar.style.width = width + "%";
+    }
+  }, 15);
+}
 
-  // Function to hide the progress bar and show the website content
-  function hideProgressBar() {
-    const progressContainer = document.getElementById("progress-container");
-    progressContainer.style.display = "none";
-  }
+//nav
+$(document).ready(function() {
+  var currentUrl = window.location.href;
 
-  updateProgressBar();
+  $(".navbar-nav .nav-link").each(function() {
+    var linkUrl = $(this).attr("href");
 
- //nav
-  $(document).ready(function() {
-  
-    var currentUrl = window.location.href;
-  
- 
-    $(".navbar-nav .nav-link").each(function() {
-      var linkUrl = $(this).attr("href");
-  
-
-      if (currentUrl.indexOf(linkUrl) !== -1) {
-     
-        $(this).addClass("active");
-      }
-    });
-    
-   
-    if (currentUrl.endsWith("index.html") || currentUrl.endsWith("/")) {
-      $(".navbar-nav .nav-link[href='index.html']").addClass("active");
+    if (currentUrl.indexOf(linkUrl) !== -1) {
+      $(this).addClass("active");
     }
   });
 
-  
+  if (currentUrl.endsWith("index.html") || currentUrl.endsWith("/")) {
+    $(".navbar-nav .nav-link[href='index.html']").addClass("active");
+  }
+});
+
+updateProgressBar();
